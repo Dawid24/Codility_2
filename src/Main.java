5d8d71cb0c6ee38258863bee0228945f24ff78f9
Dawid24 <paciorekdawidonly@gmail.com>
2018-08-19 10:09:00 +0200

    Version2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
