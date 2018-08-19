4cbd0f680b7680dcecfb155ebca87c797caa544b
Dawid24 <paciorekdawidonly@gmail.com>
2018-08-19 10:07:51 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] table = {3, 8, 9, 7, 6};
+
+        int[] tab2 = Solution.arraySolution(table, 3);
+
+        for (int i = 0; i < tab2.length; i++) {
+            System.out.print("[" + tab2[i] + "] ");
+        }
+
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Solution {
+
+    public static int[] arraySolution(int[] A, int K) {
+        LinkedList<Integer> bufor = new LinkedList<Integer>();
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (j == 0) {
+                    bufor.add(A[j + 1]);
+                    A[j + 1] = A[j];
+                } else if (j > 0 && j < A.length - 1) {
+                    bufor.add(A[j + 1]);
+                    A[j + 1] = bufor.getFirst();
+                    bufor.removeFirst();
+                } else if (j == A.length - 1) {
+                    A[0] = bufor.getFirst();
+                    bufor.clear();
+                }
+
+            }
+        }
+        return A;
+    }
+}
